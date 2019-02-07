03887ca78a79138bcfff9e1f793e696973227cb2
p-bradley <p_bradley@fanshaweonline.ca>
2019-02-06 21:59:33 -0500

    updated charts to final version after finding errors

--- assets/eggs.py
+++ assets/eggs.py
@@ -1,23 +1,25 @@
+import numpy as np
 import matplotlib.pyplot as plt
-
-n_groups = 10
-
-years = ('2014', '2015', '2016', '2017', '2018')
-
-rainbowTroutReceived = (100000, 150000, 100000, 50000, 150000)
-
-brownTroutReceived = (100000, 80000, 70000, 80000, 100000)
-
-rainbowTroutHatched = (80000, 120000, 90000, 40000, 140000)
-
-brownTroutHatched = (70000, 70000, 70000, 70000, 90000)
-
-plt.scatter(years, rainbowTroutReceived, c='b')
-plt.scatter(years, brownTroutReceived, c='#FF1493')
-plt.scatter(years, rainbowTroutHatched, c='r')
-plt.scatter(years, brownTroutHatched, c='#19b105')
-
-plt.xlabel('Years')
-plt.ylabel('Number of Eggs Received and Hatched')
-plt.title('Number of Rainbow and Brown Trout Eggs Received and Hatched Since 2014')
+ 
+barWidth = 0.15
+ 
+rainbowTroutReceived = [100000, 150000, 100000, 50000, 150000]
+brownTroutReceived = [100000, 80000, 70000, 80000, 100000]
+rainbowTroutHatched = [80000, 120000, 90000, 40000, 140000]
+brownTroutHatched = [70000, 70000, 70000, 70000, 90000]
+ 
+RTR = np.arange(len(rainbowTroutReceived))
+BTR = [x + barWidth for x in RTR]
+RTH = [x + barWidth for x in BTR]
+BTH = [x + barWidth for x in RTH]
+ 
+plt.bar(RTR, rainbowTroutReceived, color='#d08bd1', width=barWidth, label='rainbowTroutReceived')
+plt.bar(BTR, brownTroutReceived, color='#472b15', width=barWidth, label='brownTroutReceived')
+plt.bar(RTH, rainbowTroutHatched, color='#632a64', width=barWidth, label='rainbowTroutHatched')
+plt.bar(BTH, brownTroutHatched, color='#974b0e', width=barWidth, label='brownTroutHatched')
+ 
+plt.xlabel('group', fontweight='bold')
+plt.xticks([W + barWidth for W in range(len(rainbowTroutReceived))], ['2016', '2017', '2018', ''])
+ 
+plt.legend()
 plt.show()
--- assets/electro_fishing.py
+++ assets/electro_fishing.py
@@ -1,30 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
-
-n_groups = 9
-
-Komoka = (15, 14, 23)
-Oxbow = ( 10, 18, 12)
-Dingman = (13, 15, 14)
-
-fig, ax = plt.subplots()
-
-index = np.arange(n_groups)
-bar_width = 0.35
-
-opacity = 0.6
-
-ax.bar(index, Komoka, bar_width, alpha=opacity, color='b', label='Komoka')
-ax.bar(index + bar_width, Oxbow, bar_width, alpha=opacity, color='#FF1493', label='Oxbow')
-ax.bar(index + bar_width, Dingman, bar_width, alpha=opacity, color='r', label='Dingman')
-
-ax.set_xlabel('Year')
-ax.set_ylabel('Number of Rainbow Trout Caught')
-ax.set_title('Number of Rainbow Trout Caught Electro Fishing in Lake Komoka, Oxbow and Dingman Since 2016')
-ax.set_xticks(index + bar_width / 2)
-ax.set_xticklabels(('2016', '2017', '2018'))
-ax.legend()
-
-fig.tight_layout()
+ 
+barWidth = 0.25
+ 
+Komoka = [15, 14, 23,]
+Oxbow = [10, 18, 12]
+Dingman = [13, 15, 14]
+ 
+K = np.arange(len(Komoka))
+O = [x + barWidth for x in K]
+D = [x + barWidth for x in O]
+ 
+plt.bar(K, Komoka, color='r', width=barWidth, label='Komoka')
+plt.bar(O, Oxbow, color='g', width=barWidth, label='Oxbow')
+plt.bar(D, Dingman, color='b', width=barWidth, label='Dingman')
+ 
+plt.xlabel('group', fontweight='bold')
+plt.xticks([W + barWidth for W in range(len(Komoka))], ['2016', '2017', '2018',])
+ 
+plt.legend()
 plt.show()

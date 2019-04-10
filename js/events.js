7b1e097d8ee497f7128cee2eaa34e36fe9a4729d
Austin Caron <a_caron59852@fanshaweonline.ca>
2019-04-10 05:15:44 -0400

    added responsive designs for home/about for mobile/tablet

--- js/events.js
+++ js/events.js
@@ -4,6 +4,8 @@ let lightbox = document.querySelector(".lightbox"),
     lightPara = document.querySelector(".lightBoxText"),
     lightImg = document.querySelector(".lightBoxImage");
 
+
+    // the MDA for the dynamic text in the lightbox
     var	text = [ 
     [`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda excepturi debitis nostrum, 
     nemo illo magnam id voluptates eum dolor accusamus perspiciatis velit in ipsum fugiat culpa quia modi, inventore facilis!`],
@@ -17,23 +19,28 @@ let lightbox = document.querySelector(".lightbox"),
 
     projectBox.forEach(box => {
       box.addEventListener("click", function(e) {
+
+      	// this is checking for the class names of the clicked event and taking the second one
       	let i = this.className.split(" ")[1];
 
+      	// this is adding the text from the MDA that correlates to the class name found from i
+      	// it's also adding the showLightBox class from CSS 
       	lightPara.textContent = `${text[i]}`
         lightbox.classList.add("showLightBox");
       });
     });
 
     function closeLightBox() {
+    // this is removing showLightBox from the class list when clicking anywhere in the lightbox
       lightbox.classList.remove("showLightBox");
     }
 
+    // function for closing the side menu and revealing the full screen if needed.
     button.addEventListener("click", function() {
       burgerMenu.classList.remove("toggleOn");
         topNav.classList.remove("toggleOn");
           blackBar.classList.add("blackBar");
         });
-  // function for closing the side menu and revealing the full screen if needed.
 
 
     lightbox.addEventListener("click", closeLightBox);
